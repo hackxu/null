3ba4045422504d29c0062ef993689a2a5ccdaad8
bluexu <xhm520g@gamil.com>
2016-01-24 00:46:30 +0800

    数组方法

--- time/1-23/array.js
+++ time/1-23/array.js
@@ -40,15 +40,84 @@ function random(target) {
 }
 
 //对数组进行平坦化处理,返回一个一维的新数组
-function flatten(target){
+function flatten(target) { //one
     "use strict";
     var result = [];
-    target.forEach(function(item){
-        if(Array.isArray(item)){
+    target.forEach(function (item) {
+        if (Array.isArray(item)) {
             result = result.concat(flatten(item))
-        }else {
+        } else {
             result.push(item);
         }
-    })
+    });
     return result;
+}
+
+//对数组进行去重操作,返回一个以为的新数组
+function unique(target) {
+    "use strict";
+    var result = [];
+    loop: for (var i = 0, n = target.length; i < n; i++) {
+        for (var x = i + 1; x < n; x++) {
+            if (target[x] === target[i]) {
+                continue loop;
+            }
+        }
+    }
+}
+
+//过滤数组中的null与undefined,但不影响原数组
+function compact(target) {
+    "use strict";
+    return target.filter(function (el) {
+        return el != null;
+    })
+}
+
+//取得对向数组的每个元素的指定属性,组成数组返回
+function pluck(target, name) {
+    "use strict";
+    var result = [], prop;
+    target.forEach(function (item) {
+        prop = item[name];
+        if (prop != null) {
+            result.push(prop);
+        }
+    });
+    return result;
+}
+
+//根据指定条件(如回调或对象的某个属性)进行分组,构成对象返回
+function groupBy(target, val) {
+    "use strict";
+    var result = {};
+    var iterator = $.isFunction(val) ? val : function (obj) {
+        return obj[val];
+    };
+    target.forEach(function (value, index) {
+        var key = iterator(value, index);
+        (result[key] || (result[key] = [])).push(value);
+    });
+    return result;
+}
+
+//根据指定条件进行排序,通常用于对象数组
+function sortBy(target, fn, scope) {
+    "use strict";
+    var array = target.map(function (item, index) {
+        return {
+            el: item,
+            re: fn.call(scope, item, index)
+        };
+    }).sort(function (left, right) {
+        var a = left.re, b = right.re;
+        return a < b ? -1 : a > b ? 1 : 0;
+    });
+    return pluck(array, 'el');
+}
+
+//对两个数组取并集
+function union(target, array) {
+    "use strict";
+    return unique(target.concat(array))
 }
